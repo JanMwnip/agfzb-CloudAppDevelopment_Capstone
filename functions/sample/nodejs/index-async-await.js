8c435079d17b4b6fe604042247c8ed22da8a89eb
JanMwnip <janmwnip@gmail.com>
2022-06-09 13:57:23 +0000

    update to creds, async and promise

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -5,6 +5,7 @@
 const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
+/*
 async function main(params) {
       const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
       const cloudant = CloudantV1.newInstance({
@@ -17,5 +18,18 @@ async function main(params) {
       } catch (error) {
           return { error: error.description };
       }
+}
+*/
+async function main(params) {
+    const cloudant = Cloudant({
+        url: params.COUCH_URL,
+        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+    });
+    try {
+        let dbList = await cloudant.db.list();
+        return { "dbs": dbList };
+    } catch (error) {
+        return { error: error.description };
+    }
 }
 
